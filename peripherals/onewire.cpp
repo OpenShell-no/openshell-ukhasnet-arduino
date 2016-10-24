bfe4e6de4c3eca78f4731fe49100b3d754a3ac68
Odd Stråbø <oddstr13@openshell.no>
2016-10-24 10:40:34 +0200

    Copypasta fixes, search for only DS18B20, and conserve onewire_config items.

--- peripherals/onewire.cpp
+++ peripherals/onewire.cpp
@@ -5,20 +5,19 @@ OneWire onewire_obj(10);
 
 uint8_t addr[8];
 void onewire_init() {
-  onewire_obj.reset_search();
+  onewire_obj.target_search(0x28);
 
   uint8_t i=0;
-  while (ds.search(addr)) {
+  while (onewire_obj.search(addr)) {
     for (uint8_t j=0;j<8;j++) {
       onewire_config.ds[i].address[j] = addr[j];
     }
 
-    // FIXME: search for next device instead, to conserve onewire_config items
     // if device is DS18B20 and crc ok
     if (addr[0] == 0x28 and OneWire::crc8(addr, 7) == addr[7]) {
       onewire_config.ds[i].present = true;
     } else {
-      onewire_config.ds[i].present = false;
+      continue;// FIXME: search for next device instead, to conserve onewire_config items
     }
 
     if (++i < ONEWIRE_CONFIG_MAX_DEVICES) {
@@ -31,8 +30,8 @@ void onewire_init() {
   }
 }
 
-onewire_sample() {
-  ds.reset();
-  ds.write(0xCC); // Skip ROM
-  ds.write(0x44); // Convert temperature
+void onewire_sample() {
+  onewire_obj.reset();
+  onewire_obj.write(0xCC); // Skip ROM
+  onewire_obj.write(0x44); // Convert temperature
 }
